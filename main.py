46b32f5533ad01effcbf2e820a92a515d400b61d
Massimo Giardina <massimogg@protonmail.com>
2022-11-27 19:59:59 +0100

    copy/paste uit exercise3.

--- main.py
+++ main.py
@@ -18,9 +18,6 @@ from matplotlib import pyplot
 # Optimization module in scipy
 from scipy import optimize
 
-# will be used to load MATLAB mat datafile format
-from scipy.io import loadmat
-
 # library written for this exercise providing additional functions for assignment submission, and others
 import utils
 
@@ -50,18 +47,40 @@ Ik raad aan om de volgende kolommen te negeren (en dus verwijderen) aangezien ze
 - plate ?
 - MJD
 '''
-
 X = data 
+
+# TODO VANAF HIER MOET ALLES NOG AANGEPAST WORDEN OP ONZE DATASET
+
+# TODO: Test dataset toevoegen (Ik zou 80/20 nemen hiervoor?)
+# test values for the parameters theta
+theta_t = np.array([-2, -1, 1, 2], dtype=float)
+
+# test values for the inputs
+X_t = np.concatenate([np.ones((5, 1)), np.arange(1, 16).reshape(5, 3, order='F')/10.0], axis=1)
+
+# test values for the labels
+y_t = np.array([1, 0, 1, 0, 1])
+
+# test value for the regularization parameter
+lambda_t = 3
 
 # Convert string labels to classification values
 y = labels
 
 m = y.size
 
-print("Dataset size: ", m)
-print("Dataset shape: ", data.shape)
-print("input shape: ", X.shape)
-print("Labels: ", y)
-print(X[1])
+# Calculate cost function
+J, grad = utils.lrCostFunction(theta_t, X_t, y_t, lambda_t)
 
+print('Cost         : {:.6f}'.format(J))
+print('-----------------------')
+print('Gradients:')
+print(' [{:.6f}, {:.6f}, {:.6f}, {:.6f}]'.format(*grad))
 
+# Apply one-vs-all multi-classification
+lambda_ = 0.1
+all_theta = utils.oneVsAll(X, y, num_labels, lambda_)
+
+# Predict dataset
+pred = utils.predictOneVsAll(all_theta, X)
+print('Training Set Accuracy: {:.2f}%'.format(np.mean(pred == y) * 100))
--- utils.py
+++ utils.py
@@ -1,7 +1,169 @@
 import sys
 import numpy as np
 from matplotlib import pyplot
+# Optimization module in scipy
+from scipy import optimize
 
+def lrCostFunction(theta, X, y, lambda_):
+    """
+    Computes the cost of using theta as the parameter for regularized
+    logistic regression and the gradient of the cost w.r.t. to the parameters.
+    
+    Parameters
+    ----------
+    theta : array_like
+        Logistic regression parameters. A vector with shape (n, ). n is 
+        the number of features including any intercept.  
+    
+    X : array_like
+        The data set with shape (m x n). m is the number of examples, and
+        n is the number of features (including intercept).
+    
+    y : array_like
+        The data labels. A vector with shape (m, ).
+    
+    lambda_ : float
+        The regularization parameter. 
+    
+    Returns
+    -------
+    J : float
+        The computed value for the regularized cost function. 
+    
+    grad : array_like
+        A vector of shape (n, ) which is the gradient of the cost
+        function with respect to theta, at the current values of theta.
+    """
+
+    #Initialize some useful values
+    m = y.size
+    
+    # You need to return the following variables correctly
+    J = 0
+    grad = np.zeros(theta.shape)
+    
+    # ====================== YOUR CODE HERE ======================
+    # Hypothesis
+    h = sigmoid(np.dot(X, theta.T))
+
+    # Because of Hint 2
+    temp = theta 
+    temp[0] = 0   # because we don't add anything for j = 0
+
+    # Cost
+    J = (np.sum((-y*np.log(h) - (1-y)*np.log(1-h)))/ m) + (lambda_*np.sum(np.power(temp,2))/(2*m))
+
+    # Grad to the partial derivative of the cost 
+    grad = np.dot(h-y, X) / m + (lambda_ * temp / m)
+
+        
+    # =============================================================
+    return J, grad
+
+def oneVsAll(X, y, num_labels, lambda_):
+    """
+    Trains num_labels logistic regression classifiers and returns
+    each of these classifiers in a matrix all_theta, where the i-th
+    row of all_theta corresponds to the classifier for label i.
+    
+    Parameters
+    ----------
+    X : array_like
+        The input dataset of shape (m x n). m is the number of 
+        data points, and n is the number of features. Note that we 
+        do not assume that the intercept term (or bias) is in X, however
+        we provide the code below to add the bias term to X. 
+    
+    y : array_like
+        The data labels. A vector of shape (m, ).
+    
+    num_labels : int
+        Number of possible labels.
+    
+    lambda_ : float
+        The logistic regularization parameter.
+    
+    Returns
+    -------
+    all_theta : array_like
+        The trained parameters for logistic regression for each class.
+        This is a matrix of shape (K x n+1) where K is number of classes
+        (ie. `numlabels`) and n is number of features without the bias.
+    """
+    # Some useful variables
+    m, n = X.shape
+    
+    # You need to return the following variables correctly 
+    all_theta = np.zeros((num_labels, n + 1))
+
+    # Add ones to the X data matrix
+    X = np.concatenate([np.ones((m, 1)), X], axis=1)
+
+    # ====================== YOUR CODE HERE ======================
+    # From 0 to K - 1
+    for i in range(num_labels):
+        # Set Initial theta and options to minimize
+        initial_theta = np.zeros(n + 1) # (n+1) x 1 matrix
+        options = {'maxiter': 50}
+        
+        # Run minimize to obtain the optimal theta. Returns: theta in res.x and cost in res.fun
+        res = optimize.minimize(
+            lrCostFunction,
+            initial_theta,
+            (X, (y == i), lambda_), # obtain vector of 1's and 0's whether the ground truth is true/false
+            jac=True,
+            method='CG',
+            options=options
+        )
+        # Append obtained minized theta to array 
+        all_theta[i] = res.x
+
+
+    # ============================================================
+    return all_theta
+
+def predictOneVsAll(all_theta, X):
+    """
+    Return a vector of predictions for each example in the matrix X. 
+    Note that X contains the examples in rows. all_theta is a matrix where
+    the i-th row is a trained logistic regression theta vector for the 
+    i-th class. You should set p to a vector of values from 0..K-1 
+    (e.g., p = [0, 2, 0, 1] predicts classes 0, 2, 0, 1 for 4 examples) .
+    
+    Parameters
+    ----------
+    all_theta : array_like
+        The trained parameters for logistic regression for each class.
+        This is a matrix of shape (K x n+1) where K is number of classes
+        and n is number of features without the bias.
+    
+    X : array_like
+        Data points to predict their labels. This is a matrix of shape 
+        (m x n) where m is number of data points to predict, and n is number 
+        of features without the bias term. Note we add the bias term for X in 
+        this function. 
+    
+    Returns
+    -------
+    p : array_like
+        The predictions for each data point in X. This is a vector of shape (m, ).
+    """
+    m = X.shape[0];
+    num_labels = all_theta.shape[0]
+
+    # You need to return the following variables correctly 
+    p = np.zeros(m)
+
+    # Add ones to the X data matrix
+    X = np.concatenate([np.ones((m, 1)), X], axis=1)
+
+    # ====================== YOUR CODE HERE ======================
+    # p = h(theta*x)
+    #     h: sigmoid
+    p = sigmoid(np.dot(X, all_theta.T))
+
+    # Returns index of the max element
+    return np.argmax(p, axis=1)
 
 def displayData(X, example_width=None, figsize=(10, 10)):
     """
