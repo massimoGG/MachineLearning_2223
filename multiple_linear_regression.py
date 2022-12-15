ec27f00078d7605841fe7065f8055cf4b99de827
ismaelwarnants <ismael.warnants@gmail.com>
2022-12-15 14:10:03 +0100

    Multiple linear regression added, data of main split into train-valid-test. Todo: balance dataset

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -1,30 +1,8 @@
-'''
-Kaggle Dataset - Stellar Classification Dataset - SDSS17
-https://www.kaggle.com/datasets/fedesoriano/stellar-classification-dataset-sdss17
-https://www.kaggle.com/code/psycon/stars-galaxies-eda-and-classification
-
-Made by Massimo Giardina and Ismael Warnants
-'''
-
-# used for manipulating directory paths
-import os
-
-# Scientific and vector computation for python
-import numpy as np
-
-# Plotting library
-from matplotlib import pyplot
-
-# Optimization module in scipy
-from scipy import optimize
-
-# library written for this exercise providing additional functions for assignment submission, and others
-import utils
-
 import pandas as pd
-
-# Dataset 
-#labels = np.genfromtxt("MachineLearning_2223/star_classification.csv", delimiter=",", skip_header=1, usecols=17, dtype=str) # use arg names=True for headers
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import utils
 
 #loading the data
 df=pd.read_csv('MachineLearning_2223/star_classification.csv')
@@ -33,7 +11,6 @@ print(df['class'].value_counts())
 #plt.legend()
 #plt.show()
 
-#   The heatmap on raw data
 #sns.heatmap(df.corr(numeric_only=True))
 #plt.show()
 
@@ -71,27 +48,55 @@ print(df.head())
 # Split dataset 
 train, validate, test = np.split(df.sample(frac=1, random_state=42),[int(.7*len(df)), int(.85*len(df))])
 
-X = train.iloc[:,0:4]
+#g = df.groupby('class')
+#g.apply(lambda x: x.sample(g.size().min()).reset_index(drop=True))
+
+#print(g['class'].value_counts())
+
+print(train['class'].value_counts())
+print(validate['class'].value_counts())
+print(test['class'].value_counts())
+
+
+#setting the matrixes
+parameters = 10000
+X = train.iloc[:parameters,0:4]
 ones = np.ones([X.shape[0],1])
 X = np.concatenate((ones,X),axis=1)
 
-y = train.iloc[:,5].values #.values converts it from pandas.core.frame.DataFrame to numpy.ndarray
+y = train.iloc[:parameters,5:10].values #.values converts it from pandas.core.frame.DataFrame to numpy.ndarray
 theta = np.zeros([1,5])
 print(theta.shape)
 
-# Apply one-vs-all multi-classification
-lambda_ = 0.1
-print("X Shape:", X.shape, "Y Shape:", y.shape)
-all_theta = utils.oneVsAll(X, y, 3, lambda_)
+#set hyper parameters
+alpha = 0.00000001
+iters = 1000
 
-print(all_theta)
+#computecost
+def computeCost(X,y,theta):
+    tobesummed = np.power(((X @ theta.T)-y),2)
+    return np.sum(tobesummed)/(2 * len(X))
 
-# Predict dataset
-print(test.head())
-X_test = validate.iloc[:,0:5]
-y_test = validate.iloc[:,5:10].values
-print(X_test.shape)
-print(y_test.shape)
-pred = utils.predictOneVsAll(all_theta, X_test)
-print('Training Set Accuracy: {:.2f}%'.format(np.mean(pred == y_test) * 100))
+#gradient descent
+def gradientDescent(X,y,theta,iters,alpha):
+    cost = np.zeros(iters)
+    for i in range(iters):
+        theta = theta - (alpha/len(X)) * np.sum(X * (X @ theta.T - y), axis=0)
+        cost[i] = computeCost(X, y, theta)
+    
+    return theta,cost
 
+#running the gd and cost function
+g,cost = gradientDescent(X,y,theta,iters,alpha)
+print(g)
+
+finalCost = computeCost(X,y,g)
+print(finalCost)
+
+#plot the cost
+fig, ax = plt.subplots()  
+ax.plot(np.arange(iters), cost, 'r')  
+ax.set_xlabel('Iterations')  
+ax.set_ylabel('Cost')  
+ax.set_title('Error vs. Training Epoch') 
+plt.show()
